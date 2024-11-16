96d6ff82fab128b5a63517a1b2e404556d08b004
natalia.miesiac <natalia.miesiac@syzygy.pl>
2024-11-16 12:46:52 +0100

    Angular - Parking app

---
+++ src/app/index/index.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NzButtonComponent, NzButtonModule } from 'ng-zorro-antd/button';
+import { NzModalComponent, NzModalContentDirective, NzModalModule } from 'ng-zorro-antd/modal';
+import { NzFormControlComponent, NzFormDirective, NzFormItemComponent } from 'ng-zorro-antd/form';
+import { NzInputDirective, NzInputGroupComponent } from 'ng-zorro-antd/input';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-index',
+  standalone: true,
+  imports: [
+    CommonModule,
+    NzButtonComponent,
+    NzModalComponent,
+    NzModalContentDirective,
+    NzButtonModule,
+    NzModalModule,
+    NzFormDirective,
+    NzFormItemComponent,
+    NzFormControlComponent,
+    NzInputGroupComponent,
+    NzInputDirective,
+    FormsModule
+  ],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.scss',
+})
+export class IndexComponent {
+  isVisible = true;
+  isOkLoading = false;
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    this.isOkLoading = true;
+    setTimeout(() => {
+      this.isVisible = false;
+      this.isOkLoading = false;
+    }, 3000);
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+
+  submitForm() {
+    console.log('form submit');
+  }
+}
