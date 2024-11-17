fc6d8cf1f3886b2392a9d986994332758af294cf
Andrzej Duś <andrzej.dus@gmail.com>
2024-11-17 04:39:40 +0100

    Use testing component on root route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,5 @@ import { IndexComponent } from './index/index.component';
 import { TestingComponent } from './testing/testing.component';
 
 export const appRoutes: Route[] = [
-  { path: '', component: IndexComponent },
-  { path: 'test', component: TestingComponent }
+  { path: '', component: TestingComponent }
 ];
