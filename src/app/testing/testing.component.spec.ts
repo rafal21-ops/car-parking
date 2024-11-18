dca88e2e0ca5048452a49e16dfacc16fd425f1b6
Andrzej Duś <andrzej.dus@gmail.com>
2024-11-18 17:52:41 +0100

    Fix unit tests for testing component

--- src/app/testing/testing.component.spec.ts
+++ src/app/testing/testing.component.spec.ts
@@ -1,11 +1,37 @@
 import { TestBed } from '@angular/core/testing';
 import { TestingComponent } from './testing.component';
 import { RouterModule } from '@angular/router';
+import { DbProviderToken, ParkingSpotUseCasePortToken, ReservationsUseCasePortToken } from 'app/app.routes';
+import { InMemoryDataProvider } from '../../../libs/infrastructure/in-memory/in-memory';
+import { ParkingSpotUseCase } from '../../../libs/use-cases/parking-spot/parking-spot.use-case';
+import { GetReservationUseCase } from '../../../libs/use-cases/reservation/get-reservation.use-case';
 
 describe('TestingComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TestingComponent, RouterModule.forRoot([])],
+      providers: [
+        {
+          provide: DbProviderToken,
+          useFactory: () => {
+            return new InMemoryDataProvider();
+          },
+        },
+        {
+          provide: ParkingSpotUseCasePortToken,
+          useFactory: (db: any) => {
+            return new ParkingSpotUseCase(db);
+          },
+          deps: [DbProviderToken],
+        },
+        {
+          provide: ReservationsUseCasePortToken,
+          useFactory: (db: any) => {
+            return new GetReservationUseCase(db);
+          },
+          deps: [DbProviderToken],
+        },
+      ],
     }).compileComponents();
   });
 
