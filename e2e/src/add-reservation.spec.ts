3c502a8940c4f52d511c4bb33a531ffd7e14f252
Andrzej Duś <andrzej.dus@gmail.com>
2024-11-30 18:04:16 +0100

    feature: basic add reservation e2e test

---
+++ e2e/src/add-reservation.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('add reservation', async ({ page }) => {
+  await page.goto('http://localhost:4200/');
+  await page.getByRole('row', { name: '142' }).getByRole('button').click();
+  await page.getByPlaceholder('Imie i nazwisko').click();
+  await page.getByPlaceholder('Imie i nazwisko').press('ControlOrMeta+a');
+  await page.getByPlaceholder('Imie i nazwisko').fill('test');
+  await page.getByRole('button', { name: 'Zapisz' }).click();
+  await expect(page.getByRole('row', { name: 'test' })).toBeVisible();
+});
