64c0bea4febb87d89d32fff47bab6eae29b65f60
Andrzej Duś <andrzej.dus@gmail.com>
2024-12-01 00:59:24 +0100

    fix: environment import in app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { environment } from 'environments/environment';
+import { environment } from '../environments/environment';
 import { Route } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import {
