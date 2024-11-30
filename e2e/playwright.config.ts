681af3185bc292f1ac1bd51044ee0f44ff7265bb
Andrzej Duś <andrzej.dus@gmail.com>
2024-11-30 16:04:15 +0100

    feature: reduce playwright timeout to 10s

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+  timeout: 10000,
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npx nx run syzygy:serve',
